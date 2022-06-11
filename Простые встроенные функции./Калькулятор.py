d6cd83e7097cf798c2b458b1018a38efb3f09adf
main <shpilka05@mail.ru>
2022-06-11 16:18:07 +0300

    Буп

---
+++ Простые встроенные функции./Калькулятор.py
@@ -0,0 +1,13 @@
+delim = float(input())
+delit = float(input())
+doing = input()
+if doing == '+':
+    print(delim+delit)
+elif doing == '-':
+    print(delim-delit)
+elif doing == '*':
+    print(delim*delit)
+elif doing == '/':
+    print(delim/delit)
+else:
+    print(888888)
