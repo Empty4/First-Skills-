8fd165b91a7538fd7c8f4da733ebd31c0c378eb7
main <shpilka05@mail.ru>
2022-06-11 16:28:57 +0300

    Буп

---
+++ Простые встроенные функции./Четырехзначный минимум.py
@@ -0,0 +1,7 @@
+number = int(input())
+one = number%10
+two = number//10%100
+tree = number//100%1000
+four = number//1000
+if one>two>tree>four:
+    print(one,two)#а как сделать чтобы цифры были вместе?
