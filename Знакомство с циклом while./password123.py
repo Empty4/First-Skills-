b685b7586c5ccea2e493c5cc8bc0b8d102f1db32
main <shpilka05@mail.ru>
2022-06-12 18:51:57 +0300

    Буп

--- Знакомство с циклом while./password123.py
+++ Знакомство с циклом while./password123.py
@@ -1,6 +1,6 @@
 password1 = input()
 password2 = input()
-if len(password1) < 8 and password1 != password2:
+if len(password1) < 8 or password1 != password2:
     print('Короткий')
 else:
     print('OK')
