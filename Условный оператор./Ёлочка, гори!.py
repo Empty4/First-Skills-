17571f198ed36368bb33b3feb53af61f4da3ae50
main <shpilka05@mail.ru>
2022-06-17 19:43:23 +0300

    Буп

--- Условный оператор./Ёлочка, гори!.py
+++ Условный оператор./Ёлочка, гори!.py
@@ -2,7 +2,8 @@ first_str = input()
 second_str = input()
 third_str = input()
 if (
-        first_str == 'раз' or first_str == 'один') and second_str == 'два' and third_str == 'три' or first_str == '1' and second_str == '2' and third_str == '3':
+        first_str == 'раз' or first_str == 'один') and second_str == 'два' and third_str == 'три' or first_str == '1' \
+        and second_str == '2' and third_str == '3':
     print('ГОРИ')
 else:
     print('НЕ ГОРИ')
