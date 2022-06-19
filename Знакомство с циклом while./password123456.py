d3bd8682128bc423a4a47830a58a3862a46e9dd4
main <shpilka05@mail.ru>
2022-06-19 21:42:16 +0300

    Буп

--- Знакомство с циклом while./password123456.py
+++ Знакомство с циклом while./password123456.py
@@ -2,9 +2,9 @@ password1 = input()
 password2 = input()
 if len(password1) < 8:
     print('Короткий!')
-elif password1 != password2:
+elif len(password1) > 8 and password1 != password2:
     print('Различаются.')
-elif '123' in password1:
+elif len(password1) > 8 and password1 == password2 and '123' in password1:
     print('Простой!')
 else:
     print('OK')
